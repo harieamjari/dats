24135f5255d9625ac65777a7093e788249a697a1
Al-buharie <koleyis2358@gmail.com>
2021-01-29 19:40:46 +0800

    working scanner

--- src/dats.c
+++ src/dats.c
@@ -93,31 +93,11 @@ w:
 
   switch (c)
     {
-    case 'a':
-    case 'b':
-    case 'c':
-    case 'd':
-    case 'e':
-    case 'f':
-    case 'g':
-    case 'h':
-    case 'i':
-    case 'j':
-    case 'k':
-    case 'l':
-    case 'm':
-    case 'n':
-    case 'o':
-    case 'p':
-    case 'q':
-    case 'r':
-    case 's':
-    case 't':
-    case 'u':
-    case 'v':
-    case 'w':
-    case 'x':
-    case 'y':
+    case 'a': case 'b': case 'c': case 'd': case 'e':
+    case 'f': case 'g': case 'h': case 'i': case 'j':
+	case 'k': case 'l': case 'm': case 'n': case 'o':
+    case 'p': case 'q': case 'r': case 's': case 't':
+    case 'u': case 'v': case 'w': case 'x': case 'y':
     case 'z':
       {
 	int nchar;
@@ -168,8 +148,8 @@ w:
     case '}':
       return TOK_RCURLY_BRACE;
     case EOF:
-        fclose (t->fp);
-  return TOK_EOF;
+      fclose (t->fp);
+      return TOK_EOF;
     }
 
   fclose (t->fp);
